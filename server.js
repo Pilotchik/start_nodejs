b495b09564f6a498dae3b8b519a383219eb04b3a
Alex <pilotchik@gmail.com>
2014-03-27 10:50:18 +0400

    Создание программы
    Новая программа

---
+++ server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Александр on 27.03.2014.
+ */
+
+
+//Подключение базы данных со словарём
+//(расширение можно не указывать)
+var db = require('db');
+//инициализация словаря
+db.connect();
+
+//Подключение библиотеки index из папки user
+var User = require('user');
+
+//инициализация новых объектов
+var vasya = new User("Вася");
+var petya = new User("Петя");
+
+vasya.hello(petya);
